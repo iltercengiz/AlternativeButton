7b0d3f6113f2e18db123113d1c6263993ed5722b
Ilter Cengiz <iltercengiz@yahoo.com>
2015-03-03 13:33:37 +0200

    Added Reveal SDK

--- Example/Pods/Target Support Files/Pods-AlternativeButton/Pods-AlternativeButton-environment.h
+++ Example/Pods/Target Support Files/Pods-AlternativeButton/Pods-AlternativeButton-environment.h
@@ -12,3 +12,13 @@
 #define COCOAPODS_VERSION_MINOR_AlternativeButton 1
 #define COCOAPODS_VERSION_PATCH_AlternativeButton 0
 
+// Debug build configuration
+#ifdef DEBUG
+
+  // Reveal-iOS-SDK
+  #define COCOAPODS_POD_AVAILABLE_Reveal_iOS_SDK
+  #define COCOAPODS_VERSION_MAJOR_Reveal_iOS_SDK 1
+  #define COCOAPODS_VERSION_MINOR_Reveal_iOS_SDK 5
+  #define COCOAPODS_VERSION_PATCH_Reveal_iOS_SDK 1
+
+#endif
