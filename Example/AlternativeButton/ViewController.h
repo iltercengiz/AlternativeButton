886ae932fe6b272525d7c1cc775f70c432d52c9d
Ilter Cengiz <iltercengiz@yahoo.com>
2015-03-03 13:20:20 +0200

    Added class 'ViewController'

---
+++ Example/AlternativeButton/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlternativeButton
+//
+//  Created by Ilter Cengiz on 03/03/15.
+//  Copyright (c) 2015 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
