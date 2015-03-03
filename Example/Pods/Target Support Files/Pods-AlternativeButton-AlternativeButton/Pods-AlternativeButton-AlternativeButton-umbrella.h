9f2a3faffcb54beb53dc32a6c0b7528bdab41be3
Ilter Cengiz <iltercengiz@yahoo.com>
2015-03-03 13:55:44 +0200

    Updated pod to use frameworks instead of static libraries

---
+++ Example/Pods/Target Support Files/Pods-AlternativeButton-AlternativeButton/Pods-AlternativeButton-AlternativeButton-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "UIAlternativeButton.h"
+
+FOUNDATION_EXPORT double AlternativeButtonVersionNumber;
+FOUNDATION_EXPORT const unsigned char AlternativeButtonVersionString[];
+
