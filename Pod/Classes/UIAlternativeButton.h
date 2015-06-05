ea4df82d039b7ef91ce30c1be50cbf28fd95ae79
Ilter Cengiz <iltercengiz@yahoo.com>
2015-06-05 10:03:58 +0300

    Minor updates

--- Pod/Classes/UIAlternativeButton.h
+++ Pod/Classes/UIAlternativeButton.h
@@ -11,6 +11,17 @@
 IB_DESIGNABLE
 @interface UIAlternativeButton : UIButton
 
+/**
+ Amount of radius that will be applied to the image view of the button.
+ Defaults to 0.0;
+ */
 @property (nonatomic) IBInspectable CGFloat imageViewRadius;
+
+/**
+ The width of the image placed in the button.
+ Also used for height.
+ Defaults to 64.0;
+ */
+@property (nonatomic) IBInspectable CGFloat imageViewWidth;
 
 @end
--- Pod/Classes/UIAlternativeButton.m
+++ Pod/Classes/UIAlternativeButton.m
@@ -9,12 +9,6 @@
 #import "UIAlternativeButton.h"
 
 /**
- The width of the image placed in the button.
- Also used for height.
- */
-static CGFloat imageViewWidth = 64.0;
-
-/**
  Margin for both the title and the image.
  */
 static CGFloat margin = 6.0;
@@ -24,49 +18,69 @@ static CGFloat margin = 6.0;
 #pragma mark - NSObject UIKit Additions
 
 - (void)awakeFromNib {
-    
     [super awakeFromNib];
-    
     self.imageView.contentMode = UIViewContentModeScaleAspectFill;
     self.imageView.clipsToBounds = YES;
-    
-    self.imageView.layer.cornerRadius = imageViewWidth / 2.0;
-    
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _imageViewRadius = 0.0;
+        _imageViewWidth = 64.0;
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder {
+    self = [super initWithCoder:coder];
+    if (self) {
+        _imageViewRadius = 0.0;
+        _imageViewWidth = 64.0;
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        _imageViewRadius = 0.0;
+        _imageViewWidth = 64.0;
+    }
+    return self;
 }
 
 #pragma mark - Layout
 
 - (void)layoutSubviews {
-    
     [super layoutSubviews];
-    
-    self.imageView.frame = (CGRect){.origin = (CGPoint){.x = margin, .y = margin}, .size = (CGSize){.width = imageViewWidth, .height = imageViewWidth}};
-    self.imageView.center = (CGPoint){.x = CGRectGetWidth(self.frame) / 2.0, .y = imageViewWidth / 2.0 + margin};
-    
+    self.imageView.frame = (CGRect){.origin = (CGPoint){.x = margin, .y = margin}, .size = (CGSize){.width = self.imageViewWidth, .height = self.imageViewWidth}};
+    self.imageView.center = (CGPoint){.x = CGRectGetWidth(self.frame) / 2.0, .y = self.imageViewWidth / 2.0 + margin};
     [self.titleLabel sizeToFit];
-    
-    self.titleLabel.frame = (CGRect){.origin = (CGPoint){.x = margin, .y = imageViewWidth + 2 * margin}, .size = self.titleLabel.frame.size};
+    self.titleLabel.frame = (CGRect){.origin = (CGPoint){.x = margin, .y = self.imageViewWidth + 2 * margin}, .size = self.titleLabel.frame.size};
     self.titleLabel.center = (CGPoint){.x = CGRectGetWidth(self.frame) / 2.0, .y = CGRectGetHeight(self.frame) - CGRectGetHeight(self.titleLabel.frame) / 2.0 - margin};
-    
 }
 
 - (CGSize)intrinsicContentSize {
-    
     [self.titleLabel sizeToFit];
-    
     CGFloat titleLabelWidth = CGRectGetWidth(self.titleLabel.frame) + margin * 2; // margin from both sides.
     CGFloat imageViewWidth = CGRectGetWidth(self.imageView.frame) + margin * 2; // margin from both sides.
-    
     CGFloat height = CGRectGetHeight(self.imageView.frame) + CGRectGetHeight(self.titleLabel.frame) + margin * 3; // margin from top and bottom and distance between `imageView` and `titleLabel`.
-    
     return CGSizeMake(MAX(titleLabelWidth, imageViewWidth), height);
-    
 }
 
 #pragma mark - Setter
 
 - (void)setImageViewRadius:(CGFloat)imageViewRadius {
+    _imageViewRadius = imageViewRadius;
     self.imageView.layer.cornerRadius = imageViewRadius;
+}
+
+- (void)setImageViewWidth:(CGFloat)imageViewWidth {
+    _imageViewWidth = imageViewWidth;
+    self.imageView.layer.cornerRadius = imageViewWidth / 2.0;
+    [self setNeedsLayout];
+    [self invalidateIntrinsicContentSize];
 }
 
 @end
